81784d0baca761c2159cfb9aa09e9536fcf52e24
Brandon <Brandonstowe71@gmail.com>
2021-07-12 14:29:19 -0500

    added topBooks and requested them when going to /movies url

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 const express = require('express');
 const app = express();
+
+let topBooks = [
+  {
+    title: 'Harry Potter',
+    author: 'J.K. Rowling'
+  },
+  {
+    title: 'Lord of the Rings',
+    author: 'J.R.R Tolkien'
+  },
+  {
+    title: 'Twilight',
+    author: 'Stephanie Meyer'
+  },
+]
+
+app.get('/movies', (req, res) => {
+  res.json(topBooks);
+})
