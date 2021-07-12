aaef614eaa1fbf2a79a22398de973b87b5daedd6
Brandon <Brandonstowe71@gmail.com>
2021-07-12 16:01:06 -0500

    added morgan for logging

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require('express');
+const express = require('express'),
+  morgan = require('morgan');
 const app = express();
 
 let topBooks = [
@@ -16,6 +17,8 @@ let topBooks = [
   },
 ]
 
+app.use(morgan('common'));
+
 app.get('/', (req, res) => {
   res.send('Welcome to myFlix!!!');
 });
@@ -25,6 +28,11 @@ app.get('/movies', (req, res) => {
 });
 
 app.use(express.static('public'));
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 app.listen(8080, () => {
   console.log('Your app is listening on port 8080');
