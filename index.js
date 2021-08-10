fbd8130bb736379dfc0d43e49efebba6253c3d1c
Brandon <Brandonstowe71@gmail.com>
2021-08-10 09:10:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ let auth = require('./auth')(app);
 
 
 /*-local/mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });*/
-mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.find()
