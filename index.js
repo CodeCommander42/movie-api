c15f7a8111041931032b629e8b01c71079249211
Brandon <Brandonstowe71@gmail.com>
2021-08-23 12:26:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express'),
   bodyParser = require('body-parser')
 
-  const cors = require('cors');
+const cors = require('cors');
 app.use(cors());
   
 const passport = require('passport');
